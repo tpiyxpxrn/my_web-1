76916847f806fd38642b2a32ffaafa02aaaab8e1
Sittichai Seangnon <Seangnon09@gmail.com>
2024-08-06 11:32:41 +0700

    update to sky

--- my_web1/settings.py
+++ my_web1/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "my_web1.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [path.joinpath(BASE_DIR, 'templates')],
+        "DIRS": [Path.joinpath(BASE_DIR, 'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
